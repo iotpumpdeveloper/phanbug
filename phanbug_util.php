8c8586bb1787c6fe9f2f522256a0a662140654a7
jim huang <jim.javathunderbird@gmail.com>
2019-02-28 08:56:46 -0800

    use phanbug_var_debug to avoid memory limit issue

--- phanbug_util.php
+++ phanbug_util.php
@@ -1,16 +1,14 @@
 <?php
 function phanbug_inspect($variable, $isSpecificVar = false)
 {
-    ini_set('memory_limit','16M');
     print "\n\n";
     print "phanbug breakpoint:\n";
     print "variable details:\n";
     if (!$isSpecificVar) {
         $variable = array_diff_key($variable, array_flip(['GLOBALS', '_FILES', '_COOKIE', '_POST', '_GET', '_SERVER', '_ENV', '_REQUEST', 'argc', 'argv']));
-        var_dump($variable);
-        $variable = json_decode(json_encode($variable), true);
+        echo phanbug_var_debug($variable);
     } else {
-        print_r(json_decode(json_encode($variable)));
+        echo phanbug_var_debug($variable);
     }
 
     print "\n\n";
@@ -33,4 +31,77 @@ function phanbug_inspect($variable, $isSpecificVar = false)
 
     print implode("\n", $result);
     print "\n\n";
+}
+
+function phanbug_var_debug($variable,$strlen=100,$width=25,$depth=5,$i=0,&$objects = array())
+{
+    $search = array("\0", "\a", "\b", "\f", "\n", "\r", "\t", "\v");
+    $replace = array('\0', '\a', '\b', '\f', '\n', '\r', '\t', '\v');
+
+    $string = '';
+
+    switch(gettype($variable)) {
+    case 'boolean':      $string.= $variable?'true':'false'; break;
+    case 'integer':      $string.= $variable;                break;
+    case 'double':       $string.= $variable;                break;
+    case 'resource':     $string.= '[resource]';             break;
+    case 'NULL':         $string.= "null";                   break;
+    case 'unknown type': $string.= '???';                    break;
+    case 'string':
+        $len = strlen($variable);
+        $variable = str_replace($search,$replace,substr($variable,0,$strlen),$count);
+        $variable = substr($variable,0,$strlen);
+        if ($len<$strlen) $string.= '"'.$variable.'"';
+        else $string.= 'string('.$len.'): "'.$variable.'"...';
+        break;
+    case 'array':
+        $len = count($variable);
+        if ($i==$depth) $string.= 'array('.$len.') {...}';
+        elseif(!$len) $string.= 'array(0) {}';
+        else {
+            $keys = array_keys($variable);
+            $spaces = str_repeat(' ',$i*2);
+            $string.= "array($len)\n".$spaces.'{';
+            $count=0;
+            foreach($keys as $key) {
+                if ($count==$width) {
+                    $string.= "\n".$spaces."  ...";
+                    break;
+                }
+                $string.= "\n".$spaces."  [$key] => ";
+                $string.= phanbug_var_debug($variable[$key],$strlen,$width,$depth,$i+1,$objects);
+                $count++;
+            }
+            $string.="\n".$spaces.'}';
+        }
+        break;
+    case 'object':
+        $id = array_search($variable,$objects,true);
+        if ($id!==false)
+            $string.=get_class($variable).'#'.($id+1).' {...}';
+        else if($i==$depth)
+            $string.=get_class($variable).' {...}';
+        else {
+            $id = array_push($objects,$variable);
+            $array = (array)$variable;
+            $spaces = str_repeat(' ',$i*2);
+            $string.= get_class($variable)."#$id\n".$spaces.'{';
+            $properties = array_keys($array);
+            foreach($properties as $property) {
+                $name = str_replace("\0",':',trim($property));
+                $string.= "\n".$spaces."  [$name] => ";
+                $string.= phanbug_var_debug($array[$property],$strlen,$width,$depth,$i+1,$objects);
+            }
+            $string.= "\n".$spaces.'}';
+        }
+        break;
+    }
+
+    if ($i>0) return $string;
+
+    $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+    do $caller = array_shift($backtrace); while ($caller && !isset($caller['file']));
+    if ($caller) $string = $caller['file'].':'.$caller['line']."\n".$string;
+
+    return $string;
 }
